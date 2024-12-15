2a8177f3ae2ea9c72ec36c06026b96802afe71f2
Noam Karavani <noam12882@gmail.com>
2024-12-15 16:49:12 +0200

    new files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk"; // Import thunk without brackets
+import { BrowserRouter } from "react-router-dom";
 
-import rootReducer from './reducers';
-import App from './App';
-import './index.css';
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import rootReducer from "./reducers";
+import App from "./App";
+import "./index.css";
+import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
