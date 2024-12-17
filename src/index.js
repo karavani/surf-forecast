27e7f1566eb8d95509c82eb693f431e73d91b2dc
Noam Karavani <noam12882@gmail.com>
2024-12-17 23:47:19 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 import { HashRouter } from "react-router-dom";
 
-import rootReducer from "./reducers";
 import App from "./App";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
-const store = createStore(rootReducer);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
