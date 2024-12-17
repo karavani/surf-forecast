ef72f4a2332c3ce1e93006c34c915e06560453f2
Noam Karavani <noam12882@gmail.com>
2024-12-17 17:53:33 +0200

    delpoy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import MapView from './pages/MapView';
 import Spot from './pages/Spot';
@@ -7,14 +7,14 @@ import AddReview from './pages/AddReview';
 
 function App() {
   return (
-    <div>
+    <Router basename="/surf-forecast">
       <Routes>
         <Route path="/map" element={<MapView />} />
         <Route path="" element={<MapView />} />
         <Route path="/spot/:id" element={<Spot />} />
         <Route path="/add-review/:id" element={<AddReview />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
