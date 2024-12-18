be895f4cba8cdd0fedbd906de056e2a094536887
Noam Karavani <noam12882@gmail.com>
2024-12-18 10:25:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
 import MapView from './pages/MapView';
 import Spot from './pages/Spot';
 import AddReview from './pages/AddReview';
@@ -8,8 +7,8 @@ import AddReview from './pages/AddReview';
 function App() {
   return (
     <Routes>
-      <Route path="/map" element={<MapView />} />
       <Route path="" element={<MapView />} />
+      <Route path="/map" element={<MapView />} />
       <Route path="/spot/:id" element={<Spot />} />
       <Route path="/add-review/:id" element={<AddReview />} />
     </Routes>
